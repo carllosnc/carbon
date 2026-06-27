d745a30d4e240f078dfb1f59e1d2078dfe4b7041
Carlos Costa <carllos.nc@gmail.com>
2026-06-27 13:13:04 -0300

    feat: add grayscale color palette (21 tones) and M3 dark theme

---
+++ app/src/main/java/com/carbon/launcher/ui/theme/Grayscale.kt
@@ -0,0 +1,27 @@
+package com.carbon.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object Grayscale {
+    val g00 = Color(0xFF000000)
+    val g01 = Color(0xFF0D0D0D)
+    val g02 = Color(0xFF1A1A1A)
+    val g03 = Color(0xFF262626)
+    val g04 = Color(0xFF333333)
+    val g05 = Color(0xFF404040)
+    val g06 = Color(0xFF4D4D4D)
+    val g07 = Color(0xFF595959)
+    val g08 = Color(0xFF666666)
+    val g09 = Color(0xFF737373)
+    val g10 = Color(0xFF808080)
+    val g11 = Color(0xFF8C8C8C)
+    val g12 = Color(0xFF999999)
+    val g13 = Color(0xFFA6A6A6)
+    val g14 = Color(0xFFB3B3B3)
+    val g15 = Color(0xFFBFBFBF)
+    val g16 = Color(0xFFCCCCCC)
+    val g17 = Color(0xFFD9D9D9)
+    val g18 = Color(0xFFE6E6E6)
+    val g19 = Color(0xFFF2F2F2)
+    val g20 = Color(0xFFFFFFFF)
+}
